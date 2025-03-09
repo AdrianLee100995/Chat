d57012141baf0f3aec801bc42d516e15759e9c15
AdrianLee100995 <AdrianLee100995@outlook.com>
2025-03-09 21:32:05 +1000

    feature/update-history

--- ws_rs_with_rocket/src/main.rs
+++ ws_rs_with_rocket/src/main.rs
@@ -129,7 +129,55 @@ Ok(())
 
         Ok(())
     }
+}
 
+// Handle messag            // https://blog.stanko.io/do-you-really-need-websockets-343aed40aa9b
+            // and no need for reconnect later
+
+            // https://ws-rs.org/api_docs/ws/struct.Request.html
+            println!("Browser Request from {:?}", req.origin().unwrap().unwrap());
+            println!("Client found is {:?}", req.client_addr().unwrap());
+            let resp = Response::from_request(req);
+            // println!("{:?} \n", &resp);
+            resp
+        }
+
+        _ => Ok(Response::new(404, "Not Found", b"404 - Not Found".to_vec())),
+    }
+}
+
+fn on_open(&mut self, handshake: Handshake) -> Result<()> {
+    // We have a new connection, so we increment the connection counter
+    self.count.set(self.count.get() + 1);
+    let number_of_connection = self.count.get();
+
+    if number_of_connection > 5 {
+        // panic!("There are more user connection than expected.");
+    } }
+
+    // Handle messag            // https://blog.stanko.io/do-you-really-need-websockets-343aed40aa9b
+                // and no need for reconnect later
+
+                // https://ws-rs.org/api_docs/ws/struct.Request.html
+                println!("Browser Request from {:?}", req.origin().unwrap().unwrap());
+                println!("Client found is {:?}", req.client_addr().unwrap());
+                let resp = Response::from_request(req);
+                // println!("{:?} \n", &resp);
+                resp
+            }
+
+            _ => Ok(Response::new(404, "Not Found", b"404 - Not Found".to_vec())),
+        }
+    }
+
+    fn on_open(&mut self, handshake: Handshake) -> Result<()> {
+        // We have a new connection, so we increment the connection counter
+        self.count.set(self.count.get() + 1);
+        let number_of_connection = self.count.get();
+
+        if number_of_connection > 5 {
+            // panic!("There are more user connection than expected.");
+        }
     // Handle messag
 
 // https://rust-lang.github.io/async-book/getting_started/why_async.html
--- ws_rs_with_rocket/src/route/static_files.rs
+++ ws_rs_with_rocket/src/route/static_files.rs
@@ -6,3 +6,52 @@ pub fn file(file: PathBuf) -> Option<NamedFile> {
     NamedFile::open(Path::new("static/").join(file)).ok()
 }
 
+}
+
+// Handle messag            // https://blog.stanko.io/do-you-really-need-websockets-343aed40aa9b
+            // and no need for reconnect later
+
+            // https://ws-rs.org/api_docs/ws/struct.Request.html
+            println!("Browser Request from {:?}", req.origin().unwrap().unwrap());
+            println!("Client found is {:?}", req.client_addr().unwrap());
+            let resp = Response::from_request(req);
+            // println!("{:?} \n", &resp);
+            resp
+        }
+
+        _ => Ok(Response::new(404, "Not Found", b"404 - Not Found".to_vec())),
+    }
+}
+
+fn on_open(&mut self, handshake: Handshake) -> Result<()> {
+    // We have a new connection, so we increment the connection counter
+    self.count.set(self.count.get() + 1);
+    let number_of_connection = self.count.get();
+
+    if number_of_connection > 5 {
+        // panic!("There are more user connection than expected.");
+    } }
+
+    // Handle messag            // https://blog.stanko.io/do-you-really-need-websockets-343aed40aa9b
+                // and no need for reconnect later
+
+                // https://ws-rs.org/api_docs/ws/struct.Request.html
+                println!("Browser Request from {:?}", req.origin().unwrap().unwrap());
+                println!("Client found is {:?}", req.client_addr().unwrap());
+                let resp = Response::from_request(req);
+                // println!("{:?} \n", &resp);
+                resp
+            }
+
+            _ => Ok(Response::new(404, "Not Found", b"404 - Not Found".to_vec())),
+        }
+    }
+
+    fn on_open(&mut self, handshake: Handshake) -> Result<()> {
+        // We have a new connection, so we increment the connection counter
+        self.count.set(self.count.get() + 1);
+        let number_of_connection = self.count.get();
+
+        if number_of_connection > 5 {
+            // panic!("There are more user connection than expected.");
+        }
