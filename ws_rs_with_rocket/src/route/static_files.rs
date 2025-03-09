fbb3ad393d7a7d8f35562d4f4fb4ff042c10f9c2
AdrianLee100995 <AdrianLee100995@outlook.com>
2025-03-10 06:39:27 +1000

    feature/update-conv

--- ws_rs_with_rocket/src/route/static_files.rs
+++ ws_rs_with_rocket/src/route/static_files.rs
@@ -32,7 +32,43 @@ fn on_open(&mut self, handshake: Handshake) -> Result<()> {
         // panic!("There are more user connection than expected.");
     } }
 
+    // Handle messag           fn on_open(&mut self, handshake: Handshake) -> Result<()> {
+    // We have a new connection, so we increment the connection counter
+    self.count.set(self.count.get() + 1);
+    let number_of_connection = self.count.get();
+
+    if number_of_connection > 5 {
+        // panic!("There are more user connection than expected.");
+    } }
+
     // Handle messag            // https://blog.stanko.io/do-you-really-need-websockets-343aed40aa9b
+                // and no need for reconnect later
+
+                // https://ws-rs.org/api_docs/ws/struct.Request.html
+                println!("Browser Request from {:?}", req.origin().unwrap().unwrap());
+                println!("Client found is {:?}", req.client_addr().unwrap());
+                let resp = Response::from_request(req);
+                // println!("{:?} \n", &resp);
+                resp
+            }fn on_open(&mut self, handshake: Handshake) -> Result<()> {
+                // We have a new connection, so we increment the connection counter
+                self.count.set(self.count.get() + 1);
+                let number_of_connection = self.count.get();
+            
+                if number_of_connection > 5 {
+                    // panic!("There are more user connection than expected.");
+                } }
+            
+                // Handle messag            // https://blog.stanko.io/do-you-really-need-websockets-343aed40aa9b
+                            // and no need for reconnect later
+            
+                            // https://ws-rs.org/api_docs/ws/struct.Request.html
+                            println!("Browser Request from {:?}", req.origin().unwrap().unwrap());
+                            println!("Client found is {:?}", req.client_addr().unwrap());
+                            let resp = Response::from_request(req);
+                            // println!("{:?} \n", &resp);
+                            resp
+                        } // https://blog.stanko.io/do-you-really-need-websockets-343aed40aa9b
                 // and no need for reconnect later
 
                 // https://ws-rs.org/api_docs/ws/struct.Request.html
